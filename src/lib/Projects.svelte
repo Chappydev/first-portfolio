f6dd9882678de7fcfede8bdb219b2cce6fae6c0f
Chappydev <danjtchapman@gmail.com>
2023-03-30 22:12:30 -0600

    Add image for GoalTree project

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -13,7 +13,7 @@
         'I had no experience with tree structures before this project, so I learned a lot about how to work with trees. Representing the tree structure in the database proved especially challenging, requiring a lot of thought and iteration before landing on a working solution.',
         'The lines between tree nodes gave me a lot of trouble throughout the project. When trying to animate the color change of the line, I found a limitation in the capabilities of CSS, so I implemented the animation by combining a custom hook with the svg \'linearGradient\' element'
       ],
-      img: 'https://picsum.photos/id/36/400/600',
+      img: '../../public/GoalTree.png',
       github: 'https://github.com/Chappydev/goal-tree',
       link: 'https://goaltree.onrender.com'
     }
@@ -29,9 +29,14 @@
 </section>
 
 <style lang="scss">
+  @use './partials/mixins' as m;
+
   section {
     display: flex;
     flex-direction: column;
     align-items: center;
+    @include m.for-size(m-up) {
+
+    }
   }
 </style>
